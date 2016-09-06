e63339497606023c9f944d85699722252c4a31f5
Robin Millette <robin@millette.info>
2016-09-06 14:22:33 -0400

    Put repos in db (from json file).

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// core
+const fs = require('fs')
+
+// npm
+require('dotenv-safe').load()
+const got = require('got')
+const fetchRepos = require('rollodeqc-gh-repos')
+const pify = require('pify')
+const db = require('nano')('http://localhost:5984/repos')
+
+const bulk = pify(db.bulk)
+
+const withID = (doc) => {
+  doc._id = 'repo:' + doc.id
+  return doc
+}
+
+const putRepos = (data) => {
+  const docs = { docs: data.map(withID) }
+  return bulk(docs)
+}
+
+putRepos(require('./repos/dfcreative-repos.json'))
+  .then(console.log)
+  .catch(console.error)
