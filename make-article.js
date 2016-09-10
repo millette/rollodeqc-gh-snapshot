4c8e5625ef2f50ce3206d96c45c751a590cc7365
Robin Millette <robin@millette.info>
2016-09-10 19:13:17 -0400

    Small refactoring.

--- make-article.js
+++ make-article.js
@@ -16,21 +16,21 @@ const dbView = pify(db.view)
 const readFile = pify(fs.readFile)
 const fullNamesView = dbView.bind(null, 'app', 'fullNames')
 
-const getRepo = (login, repo) =>
-  fullNamesView({ key: [login, repo], include_docs: true })
+const getRepo = (login, repo) => fullNamesView({ key: [login, repo], include_docs: true })
+const justDate = (x) => x.split('T')[0]
 
-const makeArticle = (login, repo) => Promise.all([
-  getRepo(login, repo),
-  readFile(`articles/${repo}.md`, 'utf-8')
-])
-  .then((x2) => {
-    const doc = x2[0].rows[0].doc
-    doc.created_at = doc.created_at.split('T')[0]
-    doc.updated_at = doc.updated_at.split('T')[0]
-    if (doc.pushed_at) { doc.pushed_at = doc.pushed_at.split('T')[0] }
-    doc.md = marked(x2[1])
-    return tpl['repo-infobox'](doc)
-  })
+const massage = (x2) => {
+  const doc = x2[0].rows[0].doc
+  doc.created_at = justDate(doc.created_at)
+  doc.updated_at = justDate(doc.updated_at)
+  if (doc.pushed_at) { doc.pushed_at = justDate(doc.pushed_at) }
+  doc.md = marked(x2[1])
+  return tpl['repo-infobox'](doc)
+}
+
+const makeArticle = (login, repo) =>
+  Promise.all([getRepo(login, repo), readFile(`articles/${repo}.md`, 'utf-8')])
+    .then(massage)
 
 makeArticle('nebez', 'floppybird')
   .then((z) => {
