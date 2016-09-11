40f65f709cb385e0d958d939d489b85a158de4a7
Robin Millette <robin@millette.info>
2016-09-10 23:53:07 -0400

    make-article takes a short repo name and does the rest.

--- make-article.js
+++ make-article.js
@@ -19,19 +19,27 @@ const getRepo = (login, repo) => fullNamesView({ key: [login, repo], include_doc
 const justDate = (x) => x.split('T')[0]
 
 const massage = (x2) => {
-  const doc = x2[0].rows[0].doc
+  const doc = x2[1].rows[0].doc
   doc.created_at = justDate(doc.created_at)
   doc.updated_at = justDate(doc.updated_at)
   if (doc.pushed_at) { doc.pushed_at = justDate(doc.pushed_at) }
-  doc.md = marked(x2[1])
+  doc.md = marked(x2[0])
   return tpl['repo-infobox'](doc)
 }
 
-const makeArticle = (login, repo) =>
-  Promise.all([getRepo(login, repo), readFile(`articles/${repo}.md`, 'utf-8')])
-    .then(massage)
+const findLink = (repo, z) => {
+  const b = z.match(new RegExp(`github\.com\/(.+)\/${repo}>`, 'g'))
+  if (!b) { throw new Error('no github repo link found') }
+  return [b[0].split('/')[1], z]
+}
 
-makeArticle('nebez', 'floppybird')
+const makeArticle = (repo) => readFile(`articles/${repo}.md`, 'utf-8')
+  .then(findLink.bind(null, repo))
+  .then((login) => Promise.all([login[1], getRepo(login[0], repo)]))
+  .then(massage)
+
+
+makeArticle('floppybird') // floppybird libstreaming apell
   .then((z) => {
     console.log(z)
   })
